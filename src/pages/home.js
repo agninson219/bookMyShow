72874f2996532756cd0aa4957da8ad44a2d89248
Agni <1805457@kiit.ac.in>
2021-05-13 20:29:05 +0530

    created input and fetch response from api

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Mainpagelayout from '../components/Mainpagelayout';
 
-const home = () => {
-  return <Mainpagelayout>This is Home page</Mainpagelayout>;
+const Home = () => {
+  const [input, setInput] = useState('');
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSearch = () => {
+    // http://api.tvmaze.com/search/shows?q=girls;
+    fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json()) // request sends a response object so we need to convert it into json format..
+      .then(result => {
+        console.log(result);
+      });
+    //  to call any browser API...and the fetch function return promise so we need to await it.
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+  return (
+    <Mainpagelayout>
+      This is Home page
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </Mainpagelayout>
+  );
 };
 
-export default home;
+export default Home;
