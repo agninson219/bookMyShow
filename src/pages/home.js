f4ca98a506b26193513bae51691c71d4ee080070
Agni <1805457@kiit.ac.in>
2021-05-13 21:19:12 +0530

    printed the results and a small fetch re-factor

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import Mainpagelayout from '../components/Mainpagelayout';
+import { apiGet } from '../misc/config';
 
 const Home = () => {
   const [input, setInput] = useState('');
+  const [results, setResults] = useState(null);
 
   const onInputChange = ev => {
     setInput(ev.target.value);
@@ -10,11 +12,12 @@ const Home = () => {
 
   const onSearch = () => {
     // http://api.tvmaze.com/search/shows?q=girls;
-    fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json()) // request sends a response object so we need to convert it into json format..
-      .then(result => {
-        console.log(result);
-      });
+    const queryString = `/search/shows?q=${input}`;
+    apiGet(queryString).then(result => {
+      //  here we encapsulated in api fetch logic
+      setResults(result);
+    });
+
     //  to call any browser API...and the fetch function return promise so we need to await it.
   };
 
@@ -22,6 +25,21 @@ const Home = () => {
     if (ev.keyCode === 13) {
       onSearch();
     }
+  };
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No Results</div>;
+    }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+    return null;
   };
   return (
     <Mainpagelayout>
@@ -35,6 +53,7 @@ const Home = () => {
       <button type="button" onClick={onSearch}>
         Search
       </button>
+      {renderResults()}
     </Mainpagelayout>
   );
 };
