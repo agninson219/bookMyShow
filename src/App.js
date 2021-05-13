e71b1abfd948fa8784f9d69974ae0843ff7939e9
Agni <1805457@kiit.ac.in>
2021-05-13 13:23:11 +0530

    Created routes for the App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+// import logo from "./logo.svg";
+import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <>This is home page</>
+      </Route>
+      <Route exact path="/starred">
+        <>This is Starred page</>
+      </Route>
+      {/* for default case we use only Route same as that if switch case  */}
+      <Route>
+        <>404 error</>
+      </Route>
+    </Switch>
   );
 }
 
