c021670b147395365db9cfd474a6c399213739a4
Agni <1805457@kiit.ac.in>
2021-05-15 22:00:38 +0530

    added the loading or mounted effect and error messages to show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,12 +5,34 @@ import { apiGet } from '../misc/config';
 const Show = () => {
   const { id } = useParams();
   const [show, setShow] = useState(null);
+  const [isLoading, setisLoading] = useState('true');
+  const [error, setError] = useState(null);
   useEffect(() => {
-    apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-      setShow(results);
-    });
+    let isMounted = true;
+    apiGet(`shows/${id}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          if (isMounted) {
+            setShow(results);
+            setisLoading(false);
+          }
+        });
+      }, 2000)
+
+      .catch(err => {
+        if (isMounted) {
+          setError(err.message);
+          setisLoading(false);
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
   }, [id]);
   console.log('show', show);
+  if (isLoading) return <div>Data is being loaded</div>;
+  if (error) return <div>Error ocurred :{error}</div>;
   return <div>This is the show page</div>;
 };
 
