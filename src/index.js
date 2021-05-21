609888d7c5112b87ad5dc50f5c04a764d5c448dd
Agni <1805457@kiit.ac.in>
2021-05-21 19:56:54 +0530

    again change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorkerRegistration';
+import * as serviceWorker from './service-worker';
 
 ReactDOM.render(
   <HashRouter>
